ac25c1e504aec57a8b7c772cbbda7f15291f2f11
anna <imannamj@gmail.com>
2023-10-06 20:17:14 +0900

    update...^^

---
+++ android/app/src/main/kotlin/com/example/everytime_clonecoding/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.everytime_clonecoding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'HomeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+    ));
+    return MaterialApp(
+      title: 'EveryTime CloneCoding',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: ,
+    );
+  }
+}*/
